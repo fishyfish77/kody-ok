c0e2b0a0eac3dda63600c8974efc69e232d5957d
fishyfish77 <cringekidd2137@gmail.com>
2025-09-22 17:43:14 +0200

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,16 @@
+# import time
+# inp = int(input())
+# while inp > 0:
+#     time.sleep(1)
+#     inp -= 1
+#     print(inp)
+# print("pif pif pif paf")
+
+import time
+inp = int(input())
+i = 0
+while i < inp:
+    time.sleep(1)
+    i += 1
+    print(i)
+print("pif pif pif paf")
